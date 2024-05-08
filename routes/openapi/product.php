504789e6487e9d8ba453cad9b164baac72290315
coderzhao <zhaohao731869706@163.com>
2024-05-08 18:08:38 +0800

    fix: upload demo routes

---
+++ routes/openapi/product.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of hyperf-tt.
+ *
+ * @link     https://github.com/zhaohao19941221/hyperf-tt
+ * @document https://github.com/zhaohao19941221/hyperf-tt.git
+ *
+ *
+ */
+use Hyperf\HttpServer\Router\Router;
+
+Router::get('/test1', function () {
+    return '';
+});
