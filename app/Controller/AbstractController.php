a1660b7e55171073021488c62719040f09e803d9
coderzhao <zhaohao731869706@163.com>
2024-05-20 10:11:39 +0800

    feat: 新增json响应体方法

--- app/Controller/AbstractController.php
+++ app/Controller/AbstractController.php
@@ -12,7 +12,9 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Constants\ErrorCode;
 use Hyperf\Di\Annotation\Inject;
+use Hyperf\HttpMessage\Cookie\Cookie;
 use Hyperf\HttpServer\Contract\RequestInterface;
 use Hyperf\HttpServer\Contract\ResponseInterface;
 use Psr\Container\ContainerInterface;
@@ -27,4 +29,51 @@ abstract class AbstractController
 
     #[Inject]
     protected ResponseInterface $response;
+
+    /**
+     * @Created By: zhaohao
+     * @Created At: 2024/5/20 上午9:48
+     * @Desc:成功的响应
+     */
+    protected function successResponse(array $data = [], array $cookieArr = []): \Psr\Http\Message\ResponseInterface
+    {
+        return $this->jsonResponse(0, 'success', $data, $cookieArr);
+    }
+
+    /**
+     * @Created By: zhaohao
+     * @Created At: 2024/5/20 上午9:48
+     * @Desc:错误的响应
+     * @param mixed $code
+     */
+    protected function errorResponse($code, string $msg = '', array $data = [], array $cookieArr = []): \Psr\Http\Message\ResponseInterface
+    {
+        if (empty($msg)) {
+            $msg = ErrorCode::getMessage($code);
+        }
+        return $this->jsonResponse($code, $msg, $data, $cookieArr);
+    }
+
+    /**
+     * @Created By: zhaohao
+     * @Created At: 2024/5/20 上午9:48
+     * @Desc:json响应
+     */
+    protected function jsonResponse(int $code = 0, string $message = 'success', array $data = [], array $cookieArr = [], array $headerArr = []): \Psr\Http\Message\ResponseInterface
+    {
+        $response = $this->response;
+        if (! empty($cookieArr)) {
+            foreach ($cookieArr as $cookie) {
+                if ($cookie instanceof Cookie) {
+                    $response = $response->withCookie($cookie);
+                }
+            }
+        }
+        if (! empty($headerArr)) {
+            foreach ($headerArr as $key => $val) {
+                $response = $response->withHeader($key, $val);
+            }
+        }
+        return $response->json(['code' => $code, 'msg' => $message, 'data' => $data]);
+    }
 }
